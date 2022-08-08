065f05450669a52b001d1f812d442c80aad61306
omarageorge <georgeomara8@gmail.com>
2022-08-08 11:52:10 +0300

    Fixed crossOrigin bug

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -6,7 +6,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='true'
+          />
           <link
             href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,100;1,300;1,400;1,500&display=swap'
             rel='stylesheet'
