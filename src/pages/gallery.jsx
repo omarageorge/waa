827e39c41cbb414f8aaaa96dc14857f5eb6aca0e
omarageorge <georgeomara8@gmail.com>
2022-08-14 13:55:38 +0300

    Built and styled Gallery page

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,8 +1,33 @@
+import Footer from '../components/Footer';
 import Meta from '../components/Meta';
 import NavBar from '../components/NavBar';
-import style from '../styles/gallery.module.scss';
+import Place from '../components/Place';
 
-const gallery = () => {
+function gallery() {
+  const places = [
+    { image: '/images/usa.jpg', name: 'USA' },
+
+    {
+      image: '/images/germany.jpg',
+      name: 'Germany',
+    },
+
+    {
+      image: '/images/sweden.jpg',
+      name: 'Sweden',
+    },
+
+    {
+      image: '/images/uk.jpg',
+      name: 'United Kingdom',
+    },
+
+    {
+      image: '/images/south-africa.jpg',
+      name: 'South Africa',
+    },
+  ];
+
   return (
     <>
       {/* Meta descriptions */}
@@ -11,15 +36,25 @@ const gallery = () => {
       {/* Navbar */}
       <NavBar dark />
 
-      {/* Banner section */}
-      <section id={style.banner}>
-        <div className={style.overlay}></div>
-        <div className={style.content}>
-          <h1>Gallery</h1>
-        </div>
+      {/* Page Title */}
+      <h1 className='text-gray-700 text-2xl text-center py-6 pl-10 md:text-left'>
+        Places we&apos;ve been to!
+      </h1>
+
+      {/* Pictures of places */}
+      <section className='container mx-auto h-auto grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-10 justify-items-center pb-10'>
+        {/* Place */}
+        {places.map((place) => (
+          <Place key={place.name} image={place.image} name={place.name} />
+        ))}
       </section>
+
+      {/* Footer section */}
+      <Footer />
     </>
   );
-};
+}
 
 export default gallery;
+
+// flex justify-evenly flex-wrap gap-y-10
