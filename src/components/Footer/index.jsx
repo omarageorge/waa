6a1d4407432d53a9e6d53534180df9db4498b378
omarageorge <georgeomara8@gmail.com>
2022-08-14 13:57:46 +0300

    Created footer component for used in multiple pages

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className='py-6 text-center text-white bg-black'>
+      <span className=''>
+        Copyright &copy; {new Date().getFullYear()} Westminster Atlantic
+        Airlines
+      </span>
+    </footer>
+  );
+}
