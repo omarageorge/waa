dcabe5ecd166297d0151e7e2974c6584d71ea8bf
omarageorge <georgeomara8@gmail.com>
2022-08-14 12:25:25 +0300

    Added dark property to this page Navbar

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,7 +1,23 @@
+import Meta from '../components/Meta';
+import NavBar from '../components/NavBar';
+import style from '../styles/gallery.module.scss';
+
 const gallery = () => {
   return (
     <>
-      <header></header>
+      {/* Meta descriptions */}
+      <Meta />
+
+      {/* Navbar */}
+      <NavBar dark />
+
+      {/* Banner section */}
+      <section id={style.banner}>
+        <div className={style.overlay}></div>
+        <div className={style.content}>
+          <h1>Gallery</h1>
+        </div>
+      </section>
     </>
   );
 };
